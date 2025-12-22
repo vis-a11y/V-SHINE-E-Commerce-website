8e5d574a47237f21f556025deead247599ce184a
vis432-lab <prajapativishal15537@gmail.com>
2025-12-22 13:50:26 +0530

    13

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ if(close){
     close.addEventListener('click', () => {
         nav.classList.remove('active');
     });}
+
+    // sproduct page
+    var MainImg = document.getElementById("MainImg");
+    var smallimg = document.getElementsByClassName("small-img");
+
+    smallimg[0].onclick = function() {
+        MainImg.src = smallimg[0].src;
+    }
+    smallimg[1].onclick = function() {
+        MainImg.src = smallimg[1].src;
+    }
+    smallimg[2].onclick = function() {
+        MainImg.src = smallimg[2].src;
+    }
+    smallimg[3].onclick = function() {
+        MainImg.src = smallimg[3].src;
+    }
